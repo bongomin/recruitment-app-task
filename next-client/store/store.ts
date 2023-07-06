197b3fc9aab4318ff4f041be35dadc926202d3b8
Bongomin Daniel <bongomindaniel@gmail.com>
2023-07-06 23:25:32 +0300

    comment out the slice

--- next-client/store/store.ts
+++ next-client/store/store.ts
@@ -1,21 +1,21 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import jobPostingReducer from './slices/jobPosting';
-import { createWrapper } from "next-redux-wrapper"
+// import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+// import jobPostingReducer from './slices/jobPosting';
+// import { createWrapper } from "next-redux-wrapper"
 
-export const store = configureStore({
-    reducer: {
-        jobPostingList: jobPostingReducer,
-    }
-});
+// export const store = configureStore({
+//     reducer: {
+//         jobPostingList: jobPostingReducer,
+//     }
+// });
 
 // export type AppStore = ReturnType<typeof store>;
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-    ReturnType,
-    RootState,
-    unknown,
-    Action<string>
->
+// export type AppDispatch = typeof store.dispatch;
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppThunk<ReturnType = void> = ThunkAction<
+//     ReturnType,
+//     RootState,
+//     unknown,
+//     Action<string>
+// >
 
 // export const wrapper = createWrapper<AppStore>(store)
